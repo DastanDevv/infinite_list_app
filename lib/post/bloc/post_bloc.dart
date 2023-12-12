e5a594cff5e56b5460032e5481e0a4ab8d86e633
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-12-12 21:03:27 +0600

    code block is written

--- lib/post/bloc/post_bloc.dart
+++ lib/post/bloc/post_bloc.dart
@@ -1,13 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
+import 'package:infinity_list_app/post/models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
-    on<PostEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  PostBloc({required this.httpClient}) : super(const PostState()) {}
+  final http.Client httpClient;
 }
