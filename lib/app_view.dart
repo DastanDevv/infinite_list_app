17c59cb45e9e35b073d07c6fcf66b43081cfb05e
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-12-12 21:41:38 +0600

    debugShowCheckedModeBanner => false

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:infinity_list_app/post/view/posts_page.dart';
 
 class App extends MaterialApp {
+  @override
+  bool get debugShowCheckedModeBanner => false;
   const App({super.key}) : super(home: const PostsPage());
 }
