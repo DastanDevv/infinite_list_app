df1182656af683d0a7f4bb97cda1447e9ccd5a2e
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-12-12 21:40:19 +0600

    the last commit, project is finished

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:infinity_list_app/main.dart';
+import 'package:infinity_list_app/post/view/posts_page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class App extends MaterialApp {
+  const App({super.key}) : super(home: const PostsPage());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:infinity_list_app/app/app_view.dart';
+import 'package:infinity_list_app/app_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
